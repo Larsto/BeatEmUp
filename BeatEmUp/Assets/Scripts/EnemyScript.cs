383fac2226faa67066a319b6686f37696bbabd3c
Huhtala Jussi <Jussi.Huhtala@etteplanmore.com>
2019-10-03 21:24:08 +0300

    following and hitting enemy.

---
+++ BeatEmUp/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    Animator animator;
+    public int HitPoints = 10;
+    Transform target;
+    public int Speed = 5;
+    public int MinDistance = 2;
+    public bool facingRight = true;
+    bool isAlive = true;
+    public int damage;
+    float hitTime;
+    public float setHitTime = 1;
+    float oldPosition;
+    float newPosition;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        hitTime = setHitTime;
+        oldPosition = transform.position.x;
+    }
+
+    void Update()
+    {
+        if(!isAlive)
+        {
+            return;
+        }
+        Move();
+
+        newPosition = oldPosition;
+
+        if (transform.position.x > oldPosition && !facingRight)
+            Flip();
+        if (transform.position.x < oldPosition && facingRight)
+            Flip();
+        oldPosition = transform.position.x;
+    }
+
+    private void Move()
+    {
+
+
+        if (Vector2.Distance(transform.position, target.position) > MinDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
+            animator.SetBool("Walk", true);
+        }
+        else
+        {
+            Hit();
+            animator.SetBool("Walk", false);
+        }
+
+    }
+
+    void Hit()
+    {
+        hitTime -= Time.deltaTime;
+        if(hitTime < 0)
+        animator.SetTrigger("Hit");
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public void GotHit(int amount)
+    {
+        HitPoints -= amount;
+
+        if (HitPoints > 0)
+        {
+            hitTime = setHitTime;
+            animator.SetTrigger("GotHit");
+            Debug.Log("Hitpoints " + HitPoints);
+        }
+        if (HitPoints <= 0)
+        {
+            animator.SetTrigger("Death");
+            isAlive = false;
+            Destroy(gameObject, 5);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerScript e = other.GetComponent<PlayerScript>();
+        if (e != null)
+        {
+            e.GotHit(damage);
+        }
+    }
+}
--- BeatEmUp/Assets/Scripts/PlayerScript.cs
+++ BeatEmUp/Assets/Scripts/PlayerScript.cs
@@ -4,25 +4,103 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public int maxHealth = 10;
+    int HitPoints;
     Rigidbody2D rigidbody2d;
-    float speed = 5.0f;
+    public float speed = 5.0f;
+    public bool facingRight = true;
+    bool isAlive = true;
+    Animator animator;
+    int damage;
+    public int hit1;
+    public int hit2;
 
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        HitPoints = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isAlive) { 
+        return; 
+        }
+        Walk();
+        Hit();
+    }
+
+    void Walk()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = rigidbody2d.position;
+        Vector2 move = new Vector2(horizontal, vertical);
 
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+
+        animator.SetFloat("Walk", move.magnitude);
+
+        if (horizontal > 0 && !facingRight)
+            Flip();
+        else if (horizontal < 0 && facingRight)
+            Flip();
     }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void Hit()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.SetTrigger("Hit");
+            damage = hit1;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetTrigger("PowerHit");
+            damage = hit2;
+        }
+    }
+
+    public void GotHit(int amount)
+    {
+        HitPoints -= amount;
+
+        if (HitPoints > 0)
+        {
+        //    animator.SetTrigger("GotHit");
+            Debug.Log("Hitpoints " + HitPoints);
+        }
+        if (HitPoints <= 0)
+        {
+         //   animator.SetTrigger("Death");
+            isAlive = false;
+            Destroy(gameObject, 5);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        EnemyScript e = other.GetComponent<EnemyScript>();
+        if (e != null)
+        {
+            e.GotHit(damage);
+        }
+    }
+
+
 }
